18b1f4c4cf8d92a2a17a83feff4b52b01e4dd859
askalerion <bednartd@rose-hulman.edu>
2017-06-22 17:03:58 -0400

    list working, needs reverse pipe

--- Exam1/src/app/+list/list.component.ts
+++ Exam1/src/app/+list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database";
 
 interface word {
   abbr: string;
@@ -12,14 +13,41 @@ interface word {
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+
+  readonly wordPath = 'words';
+
   word: any = {
     'abbr': '',
     'name': ''
   }
 
-  constructor() { }
+  wordStream: FirebaseListObservable<word[]>;
+
+  constructor(db: AngularFireDatabase) { 
+    this.wordStream = db.list(this.wordPath);
+  }
 
   ngOnInit() {
   }
 
+  onSubmit(): void {
+    try {
+      if (this.word.$key) {
+        this.wordStream.update(this.word.$key, this.word);
+      } else {
+        this.wordStream.push(this.word);
+      }
+
+      this.word = {
+        'abbr': '',
+        'name': ''
+      }
+    } catch (e) {
+      console.log('Form error', e);
+    }
+  }
+
+  remove(wordKey: string): void {
+    this.wordStream.remove(wordKey);
+  }
 }
